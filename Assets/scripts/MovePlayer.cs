204fb3ed91ed8825d1013036662f177481d9b799
Noah Lemen <noah.lemen@gmail.com>
2014-05-01 16:44:05 -0400

    initial commit

---
+++ Assets/scripts/MovePlayer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+	Vector3 moveVector;
+	float rY;
+	CharacterController controller;
+
+	public float movespeed = 10f;
+	public float sensitivity = 10f;
+	public float minY = -70f;
+	public float maxY = 90f;
+	public float gravity = -10f;
+
+	// Use this for initialization
+	void Awake () {
+		controller = GetComponent<CharacterController>();
+		Screen.lockCursor = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveVector = Vector3.zero;
+
+		if (Input.GetMouseButtonDown(0)) Screen.lockCursor = true;
+
+		moveVector += Input.GetAxis("Vertical") * transform.forward + Input.GetAxis("Horizontal") * transform.right;
+
+		transform.Rotate( 0f, Input.GetAxis( "Mouse X" ) * Time.deltaTime * sensitivity, 0f );
+
+		rY += Input.GetAxis( "Mouse Y" ) * Time.deltaTime * sensitivity * -1f;
+		rY = Mathf.Clamp( rY, minY, maxY );
+		Camera.main.transform.localRotation = Quaternion.Euler( rY, Camera.main.transform.localEulerAngles.y, Camera.main.transform.localEulerAngles.z );
+
+		moveVector += Vector3.up * gravity;
+		controller.Move(moveVector.normalized * movespeed * Time.deltaTime);
+
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit){
+		Rigidbody r = hit.rigidbody;
+
+		if (r == null || r.isKinematic) { return; }
+		if (hit.moveDirection.y < -0.3) { return; }
+
+		Vector3 push = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+		r.velocity = push * 2f;
+	}
+	
+}
---
+++ Assets/scripts/Sine.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;  // Needed for Math
+
+public class Sine : MonoBehaviour
+{
+	// un-optimized version
+	public float frequency = 440f;
+	public float gain = 0.05f;
+	
+	private float increment;
+	private float phase;
+	private float sampling_frequency = 44100f;
+
+	void OnAudioFilterRead(float[] data, int channels)
+	{
+		// update increment in case frequency has changed
+		increment = frequency * 2f * (float)Math.PI / sampling_frequency;
+		for (var i = 0; i < data.Length; i = i + channels)
+		{
+			phase = phase + increment;
+			// this is where we copy audio data to make them “available” to Unity
+			data[i] = (gain*(float)Math.Sin(phase));
+			// if we have stereo, we copy the mono data to each channel
+			if (channels == 2f) data[i + 1] = data[i];
+			if (phase > 2f * Math.PI) phase = 0f;
+		}
+	}
+} 
+
